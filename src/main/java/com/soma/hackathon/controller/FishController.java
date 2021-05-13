cbdc8bc2e155ed2546b204ea4a8e69c776b1d47c
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2021-05-13 16:18:05 +0900

    CORS Config

---
+++ src/main/java/com/soma/hackathon/controller/FishController.java
@@ -0,0 +1,21 @@
+package com.soma.hackathon.controller;
+
+import com.soma.hackathon.service.FishService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class FishController {
+
+    private final FishService fishService;
+
+    // CORS Test URL
+    @GetMapping("/health-check")
+    public String healthCheck() {
+        return "Healthy!";
+    }
+
+
+}
---
+++ src/main/java/com/soma/hackathon/service/FishService.java
@@ -0,0 +1,11 @@
+package com.soma.hackathon.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FishService {
+
+
+}
